74683ecc927f3ecf216a8646b133cd42e6590e3f
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-11-26 22:49:43 +0530

    Resuced width

--- Secondbrain-frontend/src/components/SideBar.tsx
+++ Secondbrain-frontend/src/components/SideBar.tsx
@@ -7,8 +7,8 @@ import { Link } from "../icons/Link";
 import { HashTag } from "../icons/HashTag";
 
 export function SideBar(){
-  return <div className="fixed h-screen w-80 border-r-2 border-gray-200 top-0">
-      <div className="flex items-center text-2xl text-purple-150 justify-between mx-10 font-bold">
+  return <div className="fixed h-screen w-72 border-r-2 border-gray-200 top-0">
+      <div className="flex items-center text-2xl text-purple-150 mx-5 my-5 font-bold gap-5">
         <Brain/>
         <div>Second Brain</div>
       </div>
--- Secondbrain-frontend/src/components/SideBarItem.tsx
+++ Secondbrain-frontend/src/components/SideBarItem.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from "react";
 
 export function SideBarItem(props : {icon : ReactElement , text : string}){
-  return <div className="flex gap-5 m-10 cursor-pointer">
+  return <div className="flex gap-5 m-10 cursor-pointer hover:bg-gray-200 p-2 rounded">
     {props.icon}
     <div>{props.text}</div>
   </div>
