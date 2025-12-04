4237aa961bb511f4fe4b1978a36604dce07a20ce
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-05 02:33:06 +0530

    connected signup with backend

--- Secondbrain-frontend/src/components/Input.tsx
+++ Secondbrain-frontend/src/components/Input.tsx
@@ -1,5 +1,12 @@
-export function Input({placeholder} : {placeholder:string}){
+import type { Ref } from "react"
+
+interface InputProps{
+  placeholder : string,
+  ref ?: Ref<HTMLInputElement>
+}
+
+export function Input({placeholder , ref} : InputProps){
   return <div className="border text-xl my-4 rounded">
-    <input type="text" placeholder={placeholder} className="p-2"/>
+    <input type="text" placeholder={placeholder} className="p-2" ref={ref}/>
   </div>
 }
--- Secondbrain-frontend/src/pages/SignUp.tsx
+++ Secondbrain-frontend/src/pages/SignUp.tsx
@@ -1,17 +1,39 @@
+import { useRef } from "react"
 import { Button } from "../components/Button"
 import { Input } from "../components/Input"
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export function SignUp(){
+
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  async function signup(){
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    await axios.post(`${BACKEND_URL}/api/v1/signup`,{
+      username,password
+    })
+
+    if(usernameRef.current) usernameRef.current.value = "";
+    if (passwordRef.current) passwordRef.current.value = "";
+
+    alert("Signed Up");
+  }
+
+
   return <div className="h-screen w-screen flex justify-center items-center bg-gray-200">
 
     <div className="h-80 w-80 bg-white rounded-md flex flex-col items-center pt-10 gap-10">
 
       <div className="flex flex-col gap-2">
-        <Input placeholder="Username"/>
-        <Input placeholder="Password"/>
+        <Input placeholder="Username" ref={usernameRef}/>
+        <Input placeholder="Password" ref={passwordRef}/>
       </div>
 
-    <Button text="SignUp" variant="secondry" onClick={()=>{}} className="w-72 justify-center"/>
+    <Button text="SignUp" variant="secondry" onClick={signup} className="w-72 justify-center"/>
 
     </div>
 
