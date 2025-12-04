cf54f4eb5755014175c7d048dcdcc720b3954cb8
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-05 01:23:04 +0530

    changed styling and added props

--- Secondbrain-frontend/src/components/Button.tsx
+++ Secondbrain-frontend/src/components/Button.tsx
@@ -7,7 +7,9 @@ interface ButtonProps{
   text : string,
   startIcon? : ReactElement<IconProps>,
   endIcon? : ReactElement<IconProps>,
-  onClick : ()=>void
+  onClick : ()=>void,
+  className? : string,
+  loading? : boolean
 }
 
 const vatiantStyle = {
@@ -27,7 +29,7 @@ export function Button(props :ButtonProps){
   const startIcon = props.startIcon && isValidElement(props.startIcon) ? cloneElement(props.startIcon,{size}) : null;
   const endIcon = props.endIcon && isValidElement(props.endIcon) ? cloneElement(props.endIcon,{size}) : null;
 
-  return <button className={`${vatiantStyle[props.variant]} ${vaiantSize[size]} rounded-md font-semibold m-1 flex justify-between gap-2 items-center cursor-pointer hover:outline-2` } onClick={props.onClick} >
+  return <button className={`${vatiantStyle[props.variant]} ${vaiantSize[size]} rounded-md font-semibold m-1 flex justify-between gap-2 items-center cursor-pointer hover:outline-2 ${props.className} ${props.loading ? "opacity-60 cursor-not-allowed!" : ""}` } onClick={props.onClick} disabled={props.loading} >
     {startIcon}
     {props.text}
     {endIcon}
--- Secondbrain-frontend/src/components/SideBarItem.tsx
+++ Secondbrain-frontend/src/components/SideBarItem.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from "react";
 
 export function SideBarItem(props : {icon : ReactElement , text : string}){
-  return <div className="flex gap-5 m-10 cursor-pointer hover:bg-gray-200 p-2 rounded">
+  return <div className="flex gap-5 my-10 mx-5 cursor-pointer hover:bg-gray-200 p-2 rounded">
     {props.icon}
     <div>{props.text}</div>
   </div>
